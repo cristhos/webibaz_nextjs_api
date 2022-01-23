8a8b656c35965942d35d76bbbad0aa841da92ecc
Br CRISTAL DIBWE <cristallithos@gmail.com>
2022-01-23 21:28:49 +0200

    custom admin

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,5 +1,18 @@
+import AuthLogo from './extensions/logo.png';
+import MenuLogo from './extensions/menu-logo.png';
+import favicon from './extensions/favicon.ico';
+
 export default {
   config: {
+    // Replace the Strapi logo in auth (login) views
+    auth: {
+      logo: AuthLogo,
+    },
+   // Replace the favicon
+    head: {
+      favicon: favicon,
+    },
+    // Add a new locale, other than 'en'
     locales: [
       // 'ar',
       'fr',
@@ -28,6 +41,37 @@ export default {
       // 'zh-Hans',
       // 'zh',
     ],
+    // Replace the Strapi logo in the main navigation
+    menu: {
+      logo: MenuLogo,
+    },
+    // Override or extend the theme
+    theme: {
+      colors: {
+        alternative100: '#ffccbc',
+        alternative200: '#ffab91',
+        alternative500: '#ff5722',
+        alternative600: '#f4511e',
+        alternative700: '#e64a19',
+        danger700: '#d32f2f'
+      },
+    },
+    // Extend the translations
+    /*translations: {
+      fr: {
+        'Auth.form.email.label': 'test',
+        Users: 'Utilisateurs',
+        City: 'CITY (FRENCH)',
+        // Customize the label of the Content Manager table.
+        Id: 'ID french',
+      },
+    },*/
+   // Disable video tutorials
+    tutorials: false,
+   // Disable notifications about new Strapi releases
+    notifications: { release:  true},
   },
+
   bootstrap() {},
 };
+
